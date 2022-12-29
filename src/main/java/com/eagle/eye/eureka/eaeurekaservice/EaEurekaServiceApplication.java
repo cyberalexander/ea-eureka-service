f219305d57c7a2a75928d575c4b396f8efd91810
CyberAlexander <taurum13@gmail.com>
2022-12-29 09:22:12 +0300

    Entry point of the Eureka Service added

---
+++ src/main/java/com/eagle/eye/eureka/eaeurekaservice/EaEurekaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.eagle.eye.eureka.eaeurekaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaEurekaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EaEurekaServiceApplication.class, args);
+    }
+
+}
