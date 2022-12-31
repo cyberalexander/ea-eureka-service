0ab599b93aaae7020f558e072d2565c0627c09f9
CyberAlexander <taurum13@gmail.com>
2022-12-31 11:52:56 +0300

    Eureka Service development in progress

---
+++ src/test/java/com/eagle/eye/eureka/eaeurekaservice/EaEurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eagle.eye.eureka.eaeurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaEurekaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
