25091633b352d9b923dd6b66da1fbd5901b49bac
CyberAlexander <taurum13@gmail.com>
2023-01-25 09:43:18 +0300

    Added docker configuration to eureka service project

--- src/main/java/com/eagle/eye/eureka/eaeurekaservice/EaEurekaServiceApplication.java
+++ src/main/java/com/eagle/eye/eureka/eaeurekaservice/EaEurekaServiceApplication.java
@@ -2,8 +2,10 @@ package com.eagle.eye.eureka.eaeurekaservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EaEurekaServiceApplication {
 
     public static void main(String[] args) {
